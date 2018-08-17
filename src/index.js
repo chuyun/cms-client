81d29e19c4b36c32887735f496c1d650439c1cb4
of2554 <Jun.930720>
2018-08-17 11:37:34 +0800

    feat:集成redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import allReducer from './reducer';
+
+// redux 注入操作
+const middleware = [thunk];
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(allReducer, /* preloadedState, */ composeEnhancers(applyMiddleware(...middleware)));
+
+console.log(store.getState());
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
 registerServiceWorker();
---
+++ src/reducer/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+	httpDataReducer: require('./httpData')
+});
